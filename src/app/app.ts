825c3698709b7a45a65f0b945b432697a31dfe2e
katherinykamili <katherinykc13@gmail.com>
2026-06-12 16:13:50 -0300

    ajustar idioma e título no arquivo index.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('projeto-angular');
+  protected readonly title = signal('Hóspedes');
 }
