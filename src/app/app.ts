77cab3a08938ed823feacde37d982aae8dec931f
katherinykamili <katherinykc13@gmail.com>
2026-06-12 17:15:33 -0300

    adicionar componente Homepage com template e estilos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MeuComponente } from "./meu-componente/meu-componente";
+import { Homepage } from "./homepage/homepage";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MeuComponente],
+  imports: [RouterOutlet, MeuComponente, Homepage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
