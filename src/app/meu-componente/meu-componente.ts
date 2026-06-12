104fc2dd03b465ca2ae499d34404d83c614eb2ef
katherinykamili <katherinykc13@gmail.com>
2026-06-12 17:09:01 -0300

    adicionar propriedades de comodidades no MeuComponente

--- src/app/meu-componente/meu-componente.ts
+++ src/app/meu-componente/meu-componente.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './meu-componente.html',
   styleUrl: './meu-componente.css',
 })
-export class MeuComponente {}
+export class MeuComponente {
+
+// Comodidades
+
+quarto1: string = "Suíte Master";
+quarto2: string = "Quarto Standard";
+camas: number = 2;
+preco1: number = 380.00;
+banheiro: boolean = true;}
